a52190136931c82c2a0dcf73e60ec22964a149cd
Frank Murillo <desarrollador3@crearp.com>
2018-12-23 23:33:05 -0500

    My first version

---
+++ src/pages/Detail.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+//import { Link } from 'react-router-dom'
+import { ButtonBackToHome } from '../components/ButtonBackToHome'
+
+const API_KEY = 'e1cd600'
+
+export class Detail extends Component {
+
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.object,
+      isExact: PropTypes.bool,
+      path: PropTypes.string,
+      url: PropTypes.string
+    })
+  }
+
+  state = { movie: {} }
+
+  _fetchMovie ({ id }) {
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+    .then(res => res.json())
+    .then(movie => {
+      console.log({ movie })
+      this.setState({ movie })
+    })
+  }
+
+  _goBack () {
+    window.history.back()
+  }
+
+  componentDidMount () {
+    console.log(this.props)
+    const { movieId } = this.props.match.params
+    this._fetchMovie({ id: movieId })
+  }
+
+  render() {
+    const { Title, Poster, Actors, Metascore, Plot } =
+    this.state.movie
+
+    return(
+      //<button onClick={this._goBack}>Volver</button>
+      <div>
+        <ButtonBackToHome />        
+        <h1>{ Title }</h1>
+        <img src={Poster} alt={Poster} />
+        <h3>{ Actors }</h3>
+        <span>{ Metascore }</span>
+        <p>{ Plot }</p>
+      </div>
+    )
+  }
+}
